f8db26ff3be63099e565e83737742ade83be3152
Willian Danko Caboski <caboskidanko@gmail.com>
2023-07-25 14:25:23 -0300

    Backbones
    Criação da estrutura do projeto
    Criação de dos arquivos app.py e da pagina index

---
+++ Backend/app.py
@@ -0,0 +1,8 @@
+# 1 - Objetivo - Desenvolver uma API para a consulta, criaçção, ediõ e exclusão clientes de uma base de dados
+# 2 - URL base - localhost
+# 3 - Endpoints
+    # localhost/clientes (GET)
+    # localhost/clientes (POST)
+    # localhost/clientes/id (GET)
+    # localhost/clientes/id (PUT)
+    # localhost/clientes (DELETE)
