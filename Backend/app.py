e7831a1c2b31bf8a9cb925eeb29e8c7d7ef525ee
Willian Danko Caboski <caboskidanko@gmail.com>
2023-07-26 10:47:03 -0300

    Criação endpoints
    localhost/clientes/cnpj/ - retorna o nome do clinete em função de seu CNPJ
    
     localhost/clientes/nome/ - retorna o nome do clinete em função de seu nome

--- Backend/app.py
+++ Backend/app.py
@@ -7,20 +7,52 @@
     # localhost/clientes/id (PUT)
     # localhost/clientes (DELETE)
 
+from flask import Flask, jsonify, request
 from DAOS.clienteDAO import ClienteDAO
 from DAOS.contatoDAO import ContatoDAO
 from DAOS.enderecoDAO import EnderecoDAO
 
-end =  EnderecoDAO()
-end.delete_endereco(1)
-print(end.select_endereco(8))
 
-#cliDAO = ClienteDAO()
-#cliDAO.insert_cliente("paulo", 8)
+app = Flask(__name__)
 
-#clientDAO = ContatoDAO()
-#clientDAO.insert_contato(8,"rogerio", "gestor", 888, "aal")
-#print("oi")
-#print(clientDAO.select_contato(8))
+"""criação dos objetos para conecxão como banco de dados"""
+DAOcliente = ClienteDAO()
+DAOcontato = ContatoDAO()
+DAOendereco = EnderecoDAO()
 
-#clientDAO.delete_cliente(2)
+"""Consultar todos os nomes"""
+@app.route('/clientes',methods=['GET'])
+def nomes_clientes():
+    lis_clientes=[]
+    for i in DAOcliente.select_clientes():
+        lis_clientes.append(i[0])
+
+    if len(lis_clientes)>=1:
+        return jsonify(lis_clientes)
+    else:
+        # Caso a lista de nomes de clientes não seja encontrada, retorna uma resposta vazia
+        return jsonify([])
+
+"""Consultar cliente pelo CNPJ"""
+@app.route('/clientes/cnpj/<int:cnpj>',methods=['GET'])   
+def cons_cnpj(cnpj):
+    lis_clientes=[DAOcliente.select_clientes(cnpj)]
+    
+    if len(lis_clientes)>=1:
+        return jsonify(lis_clientes[0])
+    else:
+        # Caso a lista de nomes de clientes não seja encontrada, retorna uma resposta vazia
+        return jsonify("não foi")
+    
+    """Consultar cliente pelo nome"""
+@app.route('/clientes/nome/<string:nome>',methods=['GET'])   
+def cons_nome(nome):
+    lis_clientes=[DAOcliente.select_clientes(nome)]
+    
+    if len(lis_clientes)>=1:
+        return jsonify(lis_clientes[0])
+    else:
+        # Caso a lista de nomes de clientes não seja encontrada, retorna uma resposta vazia
+        return jsonify("não foi")
+
+app.run(port=5000,host='localhost',debug=True)
